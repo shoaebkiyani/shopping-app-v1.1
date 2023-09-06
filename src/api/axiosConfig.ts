2bc161ad0f30dc8f7308651ceaabb8d75e30c2fe
shoaebkiyani <shoaebkiyani@yahoo.com>
2023-09-06 04:51:24 +0300

    env file updated

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'production'
-  ? import.meta.env.VITE_API_URL : import.meta.env.VITE_API_LOCAL_URL
+  baseURL: import.meta.env.VITE_API_URL
 })
