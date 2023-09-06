c75c38939b72e369e3f06a6e300cbb4385074ba4
shoaebkiyani <shoaebkiyani@yahoo.com>
2023-09-06 04:03:18 +0300

    images path settings

--- src/components/layout/Featured.tsx
+++ src/components/layout/Featured.tsx
@@ -1,25 +1,40 @@
-import FeaturedTitle from './FeaturedTitle'
+import FeaturedTitle from './FeaturedTitle';
 
 function Featured() {
- 
-  return (
-    <div className='max-w-screen-2xl m-auto py-2 px-4 lg:px-0'>
-        <FeaturedTitle title = 'Featured Products'/>
-            <div className='flex xs:flex-wrap gap-2 my-4 lg:mx-4 justify-evenly sm:justify-between items-center'>
-                    <div>
-                        <img src='../public/images/featured/featured_4.jpg' alt='featured' className='xs:w-full xs:h-[250px] w-[250px]' />
-                    </div>
-                    <div>
-                        <img src='../public/images/featured/featured_2.jpg' alt='featured' className='xs:w-full xs:h-[250px] w-[250px]' />
-                    </div>
-                    <div>
-                        <img src='../public/images/featured/featured_3.jpg' alt='featured' className='xs:w-full xs:h-[250px] w-[250px]' />
-                    </div>
-                    <div>
-                        <img src='../public/images/featured/featured_5.jpg' alt='featured' className='xs:w-full xs:h-[250px] w-[250px]' />
-                    </div>            
-            </div>
-    </div>
-  )
+	return (
+		<div className='max-w-screen-2xl m-auto py-2 px-4 lg:px-0'>
+			<FeaturedTitle title='Featured Products' />
+			<div className='flex xs:flex-wrap gap-2 my-4 lg:mx-4 justify-evenly sm:justify-between items-center'>
+				<div>
+					<img
+						src='/images/featured/featured_4.jpg'
+						alt='featured'
+						className='xs:w-full xs:h-[250px] w-[250px]'
+					/>
+				</div>
+				<div>
+					<img
+						src='/images/featured/featured_2.jpg'
+						alt='featured'
+						className='xs:w-full xs:h-[250px] w-[250px]'
+					/>
+				</div>
+				<div>
+					<img
+						src='/images/featured/featured_3.jpg'
+						alt='featured'
+						className='xs:w-full xs:h-[250px] w-[250px]'
+					/>
+				</div>
+				<div>
+					<img
+						src='/images/featured/featured_5.jpg'
+						alt='featured'
+						className='xs:w-full xs:h-[250px] w-[250px]'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
-export default Featured
+export default Featured;
--- src/components/layout/ImageSlider.tsx
+++ src/components/layout/ImageSlider.tsx
@@ -1,94 +1,108 @@
-import {SetStateAction, useState, useEffect, useRef} from 'react'
+import { SetStateAction, useState, useEffect, useRef } from 'react';
 
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
-import {RxDotFilled} from 'react-icons/rx';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
 function ImageSlider() {
+	const slides = [
+		{ url: '/images/slider/slider_1.jpg' },
+		{ url: '/images/slider/slider_2.jpg' },
+		{ url: '/images/slider/slider_3.jpg' },
+	];
 
-    const slides = [
-        {url: '../../public/images/slider/slider_1.jpg'},
-        {url: '../../public/images/slider/slider_2.jpg'},
-        {url: '../../public/images/slider/slider_3.jpg'},
-    ]
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    
-    const slideRef = useRef<HTMLInputElement>(null);
-    
-    let slideInterval: NodeJS.Timer | NodeJS.Timeout | number;
+	const slideRef = useRef<HTMLInputElement>(null);
 
-    let i: number;
-  
-    useEffect(()=> {
-        slideRef.current?.addEventListener('mouseenter', pauseSlide)
-        slideRef.current?.addEventListener('mouseleave', autoPlay);
-        pauseSlide();
-        autoPlay();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-    
-    const autoPlay = () => {
-       slideInterval = setInterval(()=>{
-            if(i <= slides.length - 1){
-                dotSlide(i);
-                i = i+1
-                
-            }  else {
-                i = 0;
-            }
-        },5000)
-    }
+	let slideInterval: NodeJS.Timer | NodeJS.Timeout | number;
 
-    const pauseSlide = () => {
-        clearInterval(slideInterval);
-    }
+	let i: number;
 
-    const prevSlide = () => {
-        const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
-    }
+	useEffect(() => {
+		slideRef.current?.addEventListener('mouseenter', pauseSlide);
+		slideRef.current?.addEventListener('mouseleave', autoPlay);
+		pauseSlide();
+		autoPlay();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-    const nextSlide = () => {
-        const isLastSlide = currentIndex === slides.length - 1;
-        const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
-    }
+	const autoPlay = () => {
+		slideInterval = setInterval(() => {
+			if (i <= slides.length - 1) {
+				dotSlide(i);
+				i = i + 1;
+			} else {
+				i = 0;
+			}
+		}, 5000);
+	};
 
-    const dotSlide = (slideIndex: SetStateAction<number>) => {
-        setCurrentIndex(slideIndex);
-    }
-   
-  return (
-    <>
-    <div className='xs:flex-wrap flex sm:h-[280px] md:h-[350px] lg:h-[550px]'>
-        <div ref={slideRef} onMouseOver={pauseSlide} className='w-full xs:h-[280px] lg:h-[550px] relative group'>
-            <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='h-full bg-cover bg-no-repeat transition-all duration-500'></div>
-            
-            {/* Left Arrow */}
-            <div className='group-hover:block absolute flex top-[45%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                <BsChevronCompactLeft size={30} onClick={prevSlide} />
-            </div>
-    
-            {/* Right Arrow */}
-            <div className='group-hover:block absolute flex top-[45%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                <BsChevronCompactRight size={30} onClick={nextSlide}/>
-            </div>
+	const pauseSlide = () => {
+		clearInterval(slideInterval);
+	};
 
+	const prevSlide = () => {
+		const isFirstSlide = currentIndex === 0;
+		const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+		setCurrentIndex(newIndex);
+	};
 
-            {/* Dots */}
-            <div className='flex top-4 justify-center py-2 my-[-50px] text-zinc-200'>
-            {slides.map((_slide, slideIndex) => (
-                <div key={slideIndex} className='text-2xl cursor-pointer'><RxDotFilled onClick={() => (dotSlide(slideIndex))} /></div>
-                ))}
-            </div>
-        </div>
-        <div className='sm:h-[280px] md:h-[350px] lg:h-[550px]'>
-               <img className='w-full h-[50%]' src='../public/images/banner/banner_1.jpg' alt='banner' />
-               <img className='w-full h-[50%]' src='../public/images/banner/banner_2.jpg' alt='banner' />
-        </div>
-    </div>
-    </>
-  )
+	const nextSlide = () => {
+		const isLastSlide = currentIndex === slides.length - 1;
+		const newIndex = isLastSlide ? 0 : currentIndex + 1;
+		setCurrentIndex(newIndex);
+	};
+
+	const dotSlide = (slideIndex: SetStateAction<number>) => {
+		setCurrentIndex(slideIndex);
+	};
+
+	return (
+		<>
+			<div className='xs:flex-wrap flex sm:h-[280px] md:h-[350px] lg:h-[550px]'>
+				<div
+					ref={slideRef}
+					onMouseOver={pauseSlide}
+					className='w-full xs:h-[280px] lg:h-[550px] relative group'
+				>
+					<div
+						style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+						className='h-full bg-cover bg-no-repeat transition-all duration-500'
+					></div>
+
+					{/* Left Arrow */}
+					<div className='group-hover:block absolute flex top-[45%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+						<BsChevronCompactLeft size={30} onClick={prevSlide} />
+					</div>
+
+					{/* Right Arrow */}
+					<div className='group-hover:block absolute flex top-[45%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+						<BsChevronCompactRight size={30} onClick={nextSlide} />
+					</div>
+
+					{/* Dots */}
+					<div className='flex top-4 justify-center py-2 my-[-50px] text-zinc-200'>
+						{slides.map((_slide, slideIndex) => (
+							<div key={slideIndex} className='text-2xl cursor-pointer'>
+								<RxDotFilled onClick={() => dotSlide(slideIndex)} />
+							</div>
+						))}
+					</div>
+				</div>
+				<div className='sm:h-[280px] md:h-[350px] lg:h-[550px]'>
+					<img
+						className='w-full h-[50%]'
+						src='/images/banner/banner_1.jpg'
+						alt='banner'
+					/>
+					<img
+						className='w-full h-[50%]'
+						src='/images/banner/banner_2.jpg'
+						alt='banner'
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
-export default ImageSlider
+export default ImageSlider;
